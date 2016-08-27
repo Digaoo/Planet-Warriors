4cdbfa8764f8d84a644fd608254bfd9e6d7ba17d
Digaoo <rodrigobuenorrs@gmail.com>
2016-08-27 14:38:00 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -8,59 +8,89 @@ import java.util.Scanner;
 // Pra colocar os valores das variaveis da nave voce usa as funcoes tipo: "cliente.put_Pos_Nave_Y(int coordenadaxy)".
 
 public class Cliente extends Thread{
-  int identificador_jogador;
+  private int identificador_jogador;
 
   // Posicoes do adversario.
-  private int Pos_Nave_X_Adv;
-  private int Pos_Nave_Y_Adv;
-  private int Pos_Disparo_X_Adv;
-  private int Pos_Disparo_Y_Adv;
+  private double ang_Nave_elipse_Adv;
+  private int Pos_Disparo1_X_Adv = -50;
+  private int Pos_Disparo1_Y_Adv;
+  private int Pos_Disparo2_X_Adv = -50;
+  private int Pos_Disparo2_Y_Adv;
+  private int Pos_Disparo3_X_Adv = -50;
+  private int Pos_Disparo3_Y_Adv;
 
   // posicao da nave atual.
-  private int Pos_Nave_X;
-  private int Pos_Nave_Y;
-  private int Pos_Disparo_X;
-  private int Pos_Disparo_Y;
+  private double ang_Nave_elipse;
+  private int Pos_Disparo1_X = -50;
+  private int Pos_Disparo1_Y;
+  private int Pos_Disparo2_X = -50;
+  private int Pos_Disparo2_Y;
+  private int Pos_Disparo3_X = -50;
+  private int Pos_Disparo3_Y;
 
   Socket cliente = null;
   static DataInputStream recebe_servidor = null;
   static DataOutputStream saida = null;
 
-  public static void main(String args[]) throws IOException {
-    new Cliente().start();
-  }
-
   /////////////// Funcoes de aquisição de dados do adversario ///////////////////
-  public int get_Pos_Nave_X_Adv(){
-    return Pos_Nave_X_Adv;
+  public int get_identificador_jogador() {
+	return identificador_jogador;  
+  }
+  
+  public double get_ang_Nave_elipse_Adv(){
+    return ang_Nave_elipse_Adv;
   }
 
-  public int get_Pos_Nave_Y_Adv(){
-    return Pos_Nave_Y_Adv;
+  public int get_Pos_Disparo1_X_Adv(){
+    return Pos_Disparo1_X_Adv;
   }
 
-  public int get_Pos_Disparo_X_Adv(){
-    return Pos_Disparo_X_Adv;
+  public int get_Pos_Disparo1_Y_Adv(){
+    return Pos_Disparo1_Y_Adv;
+  }
+  
+  public int get_Pos_Disparo2_X_Adv(){
+    return Pos_Disparo2_X_Adv;
   }
 
-  public int get_Pos_Disparo_Y_Adv(){
-    return Pos_Disparo_Y_Adv;
+  public int get_Pos_Disparo2_Y_Adv(){
+    return Pos_Disparo2_Y_Adv;
+  }
+  
+  public int get_Pos_Disparo3_X_Adv(){
+    return Pos_Disparo3_X_Adv;
+  }
+
+  public int get_Pos_Disparo3_Y_Adv(){
+    return Pos_Disparo3_Y_Adv;
   }
 
   /////////// Funcoes de envio de dados da nave para o servidor ///////////
-  public void put_Pos_Nave_X_Adv(int posX){
-    Pos_Nave_X = posX;
+  public void put_ang_Nave_elipse(double ang){
+     ang_Nave_elipse = ang;
   }
 
-  public void put_Pos_Nave_Y_Adv(int posY){
-    Pos_Nave_Y = posY;
+  public void put_Pos_Disparo1_X(int posX){
+    Pos_Disparo1_X = posX;
   }
-
-  public void put_Pos_Disparo_X_Adv(int posX){
-    Pos_Disparo_X = posX;
+  public void put_Pos_Disparo1_Y(int posY){
+    Pos_Disparo1_Y = posY;
   }
-  public void put_Pos_Disparo_Y_Adv(int posY){
-    Pos_Disparo_Y = posY;
+  
+  public void put_Pos_Disparo2_X(int posX){
+    Pos_Disparo2_X = posX;
+  }
+  
+  public void put_Pos_Disparo2_Y(int posY){
+    Pos_Disparo2_Y = posY;
+  }
+  
+  public void put_Pos_Disparo3_X(int posX){
+    Pos_Disparo3_X = posX;
+  }
+  
+  public void put_Pos_Disparo3_Y(int posY){
+    Pos_Disparo3_Y = posY;
   }
 
   // thread de inicio do cliente
@@ -91,19 +121,25 @@ public class Cliente extends Thread{
     while(true){ // talvez podemos fazer parar assim que o jogo acaba, mas fazermos isso depois.
       try{
         // envia os dados ao servidor.
-        saida.writeInt(Pos_Nave_X);
-        saida.writeInt(Pos_Nave_Y);
-        saida.writeInt(Pos_Disparo_X);
-        saida.writeInt(Pos_Disparo_Y);
+        saida.writeDouble(ang_Nave_elipse);
+        saida.writeInt(Pos_Disparo1_X);
+        saida.writeInt(Pos_Disparo1_Y);
+        saida.writeInt(Pos_Disparo2_X);
+        saida.writeInt(Pos_Disparo2_Y);
+        saida.writeInt(Pos_Disparo3_X);
+        saida.writeInt(Pos_Disparo3_Y);
       }catch(IOException e){
         System.out.println(e);
       }
       try{
         // recebe os dados da outra nave do servidor.
-        Pos_Nave_X_Adv = recebe_servidor.readInt();
-        Pos_Nave_Y_Adv = recebe_servidor.readInt();
-        Pos_Disparo_X_Adv = recebe_servidor.readInt();
-        Pos_Disparo_Y_Adv = recebe_servidor.readInt();
+        ang_Nave_elipse_Adv = recebe_servidor.readDouble();
+        Pos_Disparo1_X_Adv = recebe_servidor.readInt();
+        Pos_Disparo1_Y_Adv = recebe_servidor.readInt();
+        Pos_Disparo2_X_Adv = recebe_servidor.readInt();
+        Pos_Disparo2_Y_Adv = recebe_servidor.readInt();
+        Pos_Disparo3_X_Adv = recebe_servidor.readInt();
+        Pos_Disparo3_Y_Adv = recebe_servidor.readInt();
       }catch(IOException e){
         System.out.println(e);
       }
@@ -121,3 +157,4 @@ public class Cliente extends Thread{
     }
   }
  }
+
