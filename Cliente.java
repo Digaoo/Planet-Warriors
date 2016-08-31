5b69fe330ca2a742445cbdcd19341859fe290fc0
danilown <danilownunes@gmail.com>
2016-08-31 16:14:40 -0300

    AGORA SIM! Cliente e Servidor totalmente funcionais. O problema de não início do jogo foi solucionado adicionando timers de 50 ms para que nao houvesse sobrecarga da thread e para manter a sincronia, e foi corrigo o erro de quebra de pipe que era causado pelo envio fora de ordem de alguns parâmetros.

--- Cliente.java
+++ Cliente.java
@@ -12,6 +12,8 @@ public class Cliente extends Thread{
 
   // Posicoes do adversario.
   private double ang_nave_elipse_adv;
+  private int largura_raio_nave_adv;
+  private int altura_raio_nave_adv;
   private int pos_disparo1_x_adv = -50;
   private int pos_disparo1_y_adv;
   private int pos_disparo2_x_adv = -50;
@@ -24,6 +26,8 @@ public class Cliente extends Thread{
 
   // posicao da nave atual.
   private double ang_nave_elipse;
+  private int largura_raio_nave;
+  private int altura_raio_nave;
   private int pos_disparo1_x = -50;
   private int pos_disparo1_y;
   private int pos_disparo2_x = -50;
@@ -42,8 +46,8 @@ public class Cliente extends Thread{
   static DataOutputStream saida = null;
 
   /////////////// Funcoes de aquisição de dados do adversario ///////////////////
-  public boolean get_estado_jogo_adv(){
-    return estado_jogo_adv;
+  public boolean get_estado_jogo(){
+    return estado_jogo;
   }
 
   public boolean get_pronto_para_inicio(){
@@ -65,6 +69,14 @@ public class Cliente extends Thread{
   public double get_ang_nave_elipse_adv(){
     return ang_nave_elipse_adv;
   }
+
+  public int get_largura_raio_nave_adv(){
+		return largura_raio_nave_adv;
+	}
+
+  public int get_altura_raio_nave_adv(){
+		return altura_raio_nave_adv;
+	}
 
   public int get_pos_disparo1_x_adv(){
     return pos_disparo1_x_adv;
@@ -99,17 +111,25 @@ public class Cliente extends Thread{
 		estado_jogador = estado;
 	}
 
-  public void put_vida_nave_adv(int vida){
+  public void put_vida_nave(int vida){
     vida_nave = vida;
   }
 
-  public void out_vida_planeta_adv(int vida){
+  public void out_vida_planeta(int vida){
     vida_planeta = vida;
   }
 
   public void put_ang_nave_elipse(double ang){
      ang_nave_elipse = ang;
   }
+
+  public void put_largura_raio_nave(int pos){
+		largura_raio_nave = pos;
+	}
+
+  public void put_altura_raio_nave(int pos){
+		altura_raio_nave = pos;
+	}
 
   public void put_pos_disparo1_x(int posX){
     pos_disparo1_x = posX;
@@ -142,59 +162,57 @@ public class Cliente extends Thread{
       recebe_servidor = new DataInputStream(cliente.getInputStream()); // Estabelece a entrada de dados vindos do servidor.
       saida = new DataOutputStream(cliente.getOutputStream()); // Estabelece a saida de dados para o servidor.
       identificador_jogador = recebe_servidor.readInt(); // Recebe o numero de jogador respectivo a cada cliente (vem do servidor).
-      System.out.println("identificador_jogador:  " + identificador_jogador);
-      do{ // segura o cliente enquanto ele nao esta pronto.
+      do{
+        sleep(50); // faz o envio com intervado de 50 ms para evitar a perda de sincronia e uma possivel sobrecarga da thread.
         saida.writeBoolean(estado_jogador); // envia o estado do jogador ao servidor.
       } while (!estado_jogador); // segura o cliente enquanto ele nao esta pronto.
       do { // segura o cliente que ja esta pronto enquanto o adversario nao esta pronto.
-        pronto_para_inicio = recebe_servidor.readBoolean(); // receeb do servidor se o adversario esta ou nao pronto.
+        sleep(50); // para manter a sincronia e não sobrecarregar a thread, aguarda 50 ms para nova "requisição".
+        pronto_para_inicio = recebe_servidor.readBoolean(); // recebe do servidor se o adversario esta ou nao pronto.
       } while (!pronto_para_inicio); // segura o cliente que ja esta pronto enquanto o adversario nao esta pronto.
-      pronto_para_inicio=true;
-    }catch(IOException e){};
-    do{
-      try{
-        // envia os dados ao servidor.
+      sleep(3000); // aguarda 3 segundos para inicio do servidor para manter a sincronia com o cliente.
+    }catch(IOException e){
+    }catch(InterruptedException e){};
+    try{
+      do{
+        // envia os dados do cliente atual ao servidor.
         saida.writeDouble(ang_nave_elipse);
+        saida.writeInt(altura_raio_nave);
+        saida.writeInt(largura_raio_nave);
+        saida.writeInt(vida_nave);
+        saida.writeInt(vida_planeta);
         saida.writeInt(pos_disparo1_x);
         saida.writeInt(pos_disparo1_y);
         saida.writeInt(pos_disparo2_x);
         saida.writeInt(pos_disparo2_y);
         saida.writeInt(pos_disparo3_x);
         saida.writeInt(pos_disparo3_y);
-        saida.writeInt(vida_nave);
-        saida.writeInt(vida_planeta);
         saida.writeBoolean(estado_jogo);
-      }catch(IOException e){
-        System.out.println(e);
-      }
-      try{
-        // recebe os dados da outra nave do servidor.
+
+        // recebe os dados da nave adversaria através do servidor.
         ang_nave_elipse_adv = recebe_servidor.readDouble();
+        altura_raio_nave_adv = recebe_servidor.readInt();
+        largura_raio_nave_adv = recebe_servidor.readInt();
+        vida_nave_adv = recebe_servidor.readInt();
+        vida_planeta_adv = recebe_servidor.readInt();
         pos_disparo1_x_adv = recebe_servidor.readInt();
         pos_disparo1_y_adv = recebe_servidor.readInt();
         pos_disparo2_x_adv = recebe_servidor.readInt();
         pos_disparo2_y_adv = recebe_servidor.readInt();
         pos_disparo3_x_adv = recebe_servidor.readInt();
         pos_disparo3_y_adv = recebe_servidor.readInt();
-        vida_planeta_adv = recebe_servidor.readInt();
-        vida_nave_adv = recebe_servidor.readInt();
         estado_jogo_adv = recebe_servidor.readBoolean();
-      }catch(IOException e){
-        System.out.println(e);
-      }
-      try{
-        sleep(30);
-      }catch(InterruptedException e){
-        System.out.println(e);
-      }
-    }while(estado_jogo && estado_jogo_adv);
-    // finaliza o cliente
-    try{
+
+        //sleep(30);
+      }while(estado_jogo && estado_jogo_adv);
+      // finaliza o cliente.
       saida.close();
       recebe_servidor.close();
       cliente.close();
-    }catch(IOException e){
+    }/*catch(InterruptedException e){
+      System.out.println(e);
+    }*/catch(IOException e){
       System.out.println(e);
     }
   }
- }
+}
--- Servidor.java
+++ Servidor.java
@@ -10,9 +10,9 @@ public class Servidor {
     System.out.println("Porta 12345 aberta!"); // Mensagem de sucesso.
     Socket jogador1 = servidor.accept(); // Estabelece a conexão com o jogador 1.
     System.out.println("Nova conexão com o jogador 1 " + jogador1.getInetAddress().getHostAddress()); // imprime o ip do cliente
+    new Servindo(jogador1, 0).start(); // Dá inicio à thread do jogador 1.
     Socket jogador2 = servidor.accept(); // Estabelece a conexão com o jogador 2.
     System.out.println("Nova conexão com o jogador 2 " + jogador2.getInetAddress().getHostAddress()); // imprime o ip do cliente
-    new Servindo(jogador1, 0).start(); // Dá inicio à thread do jogador 1.
     new Servindo(jogador2, 1).start(); // Dá inicio à thread do jogador 2.
   }
 }
@@ -22,6 +22,8 @@ class Servindo extends Thread {
   static DataInputStream recebe_dados_cliente[] = new DataInputStream[2];  // alocação do vetor de stream de saida de dados do servidor para os clientes;
   Socket cliente;
   private double ang_nave_elipse;
+  private int largura_raio_nave;
+  private int altura_raio_nave;
   private int pos_disparo1_x;
   private int pos_disparo1_y;
   private int pos_disparo2_x;
@@ -30,7 +32,7 @@ class Servindo extends Thread {
   private int pos_disparo3_y;
   private int vida_planeta;
   private int vida_nave;
-  private boolean pronto_para_inicio = false; // indica se os dois jogadores estão pronto para o iniciar o jogo.
+  private boolean pronto_para_inicio = false; // indica se o jogador esta pronto para o iniciar o jogo.
   static int numero_jogadores_prontos = 0;
   static boolean estado_jogo = true;
   int identificador_jogador; // 0 para jogador1 e 1 para jogador2.
@@ -49,24 +51,33 @@ class Servindo extends Thread {
     try{
       distribui_dados[identificador_jogador].writeInt(identificador_jogador); // envia a identificacao ao jogador
       do{
+        sleep(50); // aguardo de 50 ms para o envio de dados, eivtando sobrecarga da thread.
         pronto_para_inicio = recebe_dados_cliente[identificador_jogador].readBoolean(); // recebe do cliente se o jogador esta pronto para o inicio do jogo.
       }while(!pronto_para_inicio); // segura o servidor enquanto o cliente em questao nao esta pronto.
-      numero_jogadores_prontos++;
+      numero_jogadores_prontos++; // quando um jogador informa que esta pronto, informa ao outro jogador quantos jogadores estao pronto (incluindo ele mesmo se este informou que estava pronto).
       do{
-        if((identificador_jogador == 0) && (numero_jogadores_prontos == 2)) // não faz envio ao jogador que nao estiver pronto. Evitando, possivelmente, o broken pipe.
-          distribui_dados[1].writeBoolean(pronto_para_inicio); // envia o estado do jogador para seu adversario.
-        else
-          if((identificador_jogador == 1) && (numero_jogadores_prontos == 2))
-            distribui_dados[0].writeBoolean(pronto_para_inicio); // envia o estado do jogador para seu adversario.
+        sleep(50); // Aguarda 50ms para fazer o envio dos dados, para manter a sincronia e, aparentemente, nao sobrecarregar uma thread.
+        if(numero_jogadores_prontos == 2){ // não faz envio ao jogador que nao estiver pronto. Evitando, possivelmente, o broken pipe.
+          if(identificador_jogador == 0)
+            distribui_dados[1].writeBoolean(pronto_para_inicio);
+          else
+            distribui_dados[0].writeBoolean(pronto_para_inicio);
+        }
       }while(numero_jogadores_prontos < 2); // segura o servidor enquanto ambos os clientes nao estao prontos.
     }catch(IOException e){
       System.out.println(e);
+    }catch(InterruptedException e){
+      System.out.println(e);
     }
-    do{ // podemos fazer o cliente enviar que o jogo foi fechado e fechar o server. mas fazemos isso por ultimo.
-      System.out.println("identificador_jogador:  " + identificador_jogador);
-      try{
+    try{
+      sleep(3000); // aguadar o cliente pois ele começa 3 segundos depois do inicio por causa da contagem.
+      do{
         // recebe o dado enviado pelo cliente e o armazena para envio ao outro cliente.
-        ang_nave_elipse = (recebe_dados_cliente[identificador_jogador]).readDouble();
+        ang_nave_elipse = recebe_dados_cliente[identificador_jogador].readDouble();
+        altura_raio_nave = recebe_dados_cliente[identificador_jogador].readInt();
+        largura_raio_nave = recebe_dados_cliente[identificador_jogador].readInt();
+        vida_nave = recebe_dados_cliente[identificador_jogador].readInt();
+        vida_planeta = recebe_dados_cliente[identificador_jogador].readInt();
         pos_disparo1_x = recebe_dados_cliente[identificador_jogador].readInt();
         pos_disparo1_y = recebe_dados_cliente[identificador_jogador].readInt();
         pos_disparo2_x = recebe_dados_cliente[identificador_jogador].readInt();
@@ -74,13 +85,14 @@ class Servindo extends Thread {
         pos_disparo3_x = recebe_dados_cliente[identificador_jogador].readInt();
         pos_disparo3_y = recebe_dados_cliente[identificador_jogador].readInt();
         estado_jogo = recebe_dados_cliente[identificador_jogador].readBoolean();
-      }catch(IOException e){
-        System.out.println(e);
-      }
-      try{
+
         // faz o envio dos dados aos respectivos clientes.
         if(identificador_jogador == 1){
           distribui_dados[0].writeDouble(ang_nave_elipse);
+          distribui_dados[0].writeInt(altura_raio_nave);
+          distribui_dados[0].writeInt(largura_raio_nave);
+          distribui_dados[0].writeInt(vida_nave);
+          distribui_dados[0].writeInt(vida_planeta);
           distribui_dados[0].writeInt(pos_disparo1_x);
           distribui_dados[0].writeInt(pos_disparo1_y);
           distribui_dados[0].writeInt(pos_disparo2_x);
@@ -91,6 +103,10 @@ class Servindo extends Thread {
         }
         else{
           distribui_dados[1].writeDouble(ang_nave_elipse);
+          distribui_dados[1].writeInt(altura_raio_nave);
+          distribui_dados[1].writeInt(largura_raio_nave);
+          distribui_dados[1].writeInt(vida_nave);
+          distribui_dados[1].writeInt(vida_planeta);
           distribui_dados[1].writeInt(pos_disparo1_x);
           distribui_dados[1].writeInt(pos_disparo1_y);
           distribui_dados[1].writeInt(pos_disparo2_x);
@@ -99,20 +115,14 @@ class Servindo extends Thread {
           distribui_dados[1].writeInt(pos_disparo3_y);
           distribui_dados[1].writeBoolean(estado_jogo);
         }
-      }catch(IOException e){
-        System.out.println(e);
-      }
-      try{
-        sleep(30);
-      }catch(InterruptedException e){
-        System.out.println(e);
-      }
-    }while (estado_jogo);
-    try{
+      }while (estado_jogo);
+      // finaliza o servidor.
       recebe_dados_cliente[identificador_jogador].close();
       distribui_dados[identificador_jogador].close();
       cliente.close();
     }catch(IOException e){
+      System.out.println(e);
+    }catch(InterruptedException e){
       System.out.println(e);
     }
   }
